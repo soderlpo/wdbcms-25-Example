fb7b8a1a5401b08b9370e83f365ec669514ab9b3
soderlpo <soderlpo@arcada.fi>
2025-05-03 06:49:30 +0000

    front-end session commit

--- main.py
+++ main.py
@@ -7,9 +7,31 @@ PORT=8461
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+# if-sats i python
+@app.get("/if/{user_input}")
+def if_test(user_input: str):
+    message = None # None är samma som null i andra språk
+
+    if user_input == "hello" or user_input == "hi": 
+        message = user_input + " yourself!" # konkatenering med +
+    elif user_input == "goodbye" and 1 > 0:
+        message = "bye bye"
+    else:
+        message = f"I don't understand {user_input}" # f-string
+        # jfr JS backtick: `I don't understand ${user_input}`;
+
+    return { "msg": message }
+
 @app.get("/")
 def hello():
-    return { "message": "Hello FastAPI" }
+    msg = "Några populära SOA-implementationer:" # string-variabel
+    soa_protocols = [ "SOAP", "REST", "GraphQL", "gRPC" ] # list
+    my_dict = { 
+        'message': msg, 
+        'myList': soa_protocols 
+    } # dictionary
+
+    return my_dict
 
 if __name__ == "__main__":
     uvicorn.run(
