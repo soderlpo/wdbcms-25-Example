a97990e98d745d0aea2cea2fae631534849b1a1a
soderlpo <soderlpo@arcada.fi>
2025-05-03 05:18:58 +0000

    First commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-PORT=5000
+PORT=8461
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
